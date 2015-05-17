0d9e051a35374534acef7f62de50b3799e03a51e
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-17 09:19:08 -0400

    Copy headers and report the root

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
-use std::{env, process};
-use std::path::PathBuf;
+#![feature(path_ext)]
+
+use std::{env, fs, io, process};
+use std::path::{Path, PathBuf};
 
 macro_rules! cmd(
     ($name:expr) => (process::Command::new($name));
@@ -17,17 +19,54 @@ macro_rules! run(
     );
 );
 
+macro_rules! ok {
+    ($result:expr) => (
+        match $result {
+            Ok(ok) => ok,
+            Err(error) => panic!("`{}` failed with `{}`", stringify!($result), error),
+        }
+    )
+}
+
+#[allow(unused_must_use)]
 fn main() {
     let source = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join("superlu");
     let output = PathBuf::from(&get!("OUT_DIR"));
 
+    let include = output.join("include");
+    fs::create_dir(&include);
+    ok!(copy(&source.join("SRC"), &include, "h"));
+
+    let lib = output.join("lib");
+    fs::create_dir(&lib);
     run!(cmd!("make").current_dir(&source)
                      .arg("superlulib")
                      .arg("NOOPTS=-fPIC -w")
                      .arg("CFLAGS=-fPIC -w -DPRNTlevel=0")
                      .arg(&format!("SuperLUroot={}", source.display()))
-                     .arg(&format!("SUPERLULIB={}", output.join("libsuperlu.a").display())));
+                     .arg(&format!("SUPERLULIB={}", lib.join("libsuperlu.a").display())));
 
+    println!("cargo:root={}", output.display());
     println!("cargo:rustc-link-lib=static=superlu");
-    println!("cargo:rustc-link-search={}", output.display());
+    println!("cargo:rustc-link-search={}", lib.display());
+}
+
+fn copy(source: &Path, destination: &Path, extension: &str) -> io::Result<()> {
+    use std::fs::PathExt;
+    for entry in try!(fs::read_dir(source)) {
+        let path = try!(entry).path();
+        if path.is_dir() { continue }
+        match path.extension() {
+            Some(name) if name == extension => {
+                match path.file_name() {
+                    Some(name) => {
+                        try!(fs::copy(&path, destination.join(name)));
+                    },
+                    _ => {},
+                }
+            },
+            _ => {},
+        }
+    }
+    Ok(())
 }
