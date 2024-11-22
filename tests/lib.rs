319a812a38afe561f823160dc6e50dad56559acd
Henrik Stromberg <henrik@askdrq.com>
2024-11-22 09:33:37 +0100

    test for sp_dgemv added

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,7 @@
+extern crate libc;
 extern crate superlu_sys as raw;
 
+use libc::c_char;
 use raw::Dtype_t::SLU_D;
 use raw::Mtype_t::SLU_GE;
 use raw::Stype_t::SLU_DN;
@@ -262,5 +264,101 @@ fn test_read_write_super_matrix_f64() {
     };
     for i in 0..v.len() as i32 {
         assert_eq!(v[i as usize], i.into());
+    }
+}
+
+#[allow(non_snake_case)]
+#[test]
+fn test_sp_dgemv() {
+    use raw::Dtype_t::*;
+    use raw::Mtype_t::*;
+    use raw::Stype_t::*;
+    use std::ffi::CString;
+
+    unsafe {
+        let m = 3;
+        let n = 3;
+        let nnz = 4;
+
+        let a = doubleMalloc(nnz);
+        assert!(!a.is_null());
+        {
+            let a = from_raw_parts_mut(a, nnz as usize);
+            a[0] = 1.0;
+            a[1] = 2.0;
+            a[2] = 3.0;
+            a[3] = 4.0;
+        }
+
+        let asub = intMalloc(nnz);
+        assert!(!asub.is_null());
+        {
+            let asub = from_raw_parts_mut(asub, nnz as usize);
+            asub[0] = 0;
+            asub[1] = 1;
+            asub[2] = 2;
+            asub[3] = 0;
+        }
+
+        let xa = intMalloc(n + 1);
+        assert!(!xa.is_null());
+        {
+            let xa = from_raw_parts_mut(xa, (n + 1) as usize);
+            xa[0] = 0;
+            xa[1] = 1;
+            xa[2] = 2;
+            xa[3] = 4;
+        }
+
+        let mut A: SuperMatrix = std::mem::zeroed();
+        dCreate_CompCol_Matrix(&mut A, m, n, nnz, a, asub, xa, SLU_NC, SLU_D, SLU_GE);
+
+        let x = doubleMalloc(n);
+        assert!(!x.is_null());
+        {
+            let x = from_raw_parts_mut(x, n as usize);
+            x[0] = 1.0;
+            x[1] = 1.0;
+            x[2] = 1.0;
+        }
+
+        let raw_y = doubleMalloc(m);
+        assert!(!raw_y.is_null());
+
+        let y_slice = from_raw_parts_mut(raw_y, m as usize);
+        for i in 0..m as usize {
+            y_slice[i] = 0.0;
+        }
+
+        let alpha = 1.0;
+        let beta = 0.0;
+
+        let trans = CString::new("N").unwrap();
+
+        let incx = 1;
+        let incy = 1;
+
+        let info = sp_dgemv(
+            trans.as_ptr() as *mut c_char,
+            alpha,
+            &mut A,
+            x,
+            incx,
+            beta,
+            raw_y,
+            incy,
+        );
+        assert_eq!(info, 0);
+
+        let y = from_raw_parts_mut(raw_y, m as usize);
+        assert_eq!(y[0], 5.0);
+        assert_eq!(y[1], 2.0);
+        assert_eq!(y[2], 3.0);
+
+        SUPERLU_FREE(a as *mut _);
+        SUPERLU_FREE(asub as *mut _);
+        SUPERLU_FREE(xa as *mut _);
+        SUPERLU_FREE(x as *mut _);
+        SUPERLU_FREE(raw_y as *mut _);
     }
 }
